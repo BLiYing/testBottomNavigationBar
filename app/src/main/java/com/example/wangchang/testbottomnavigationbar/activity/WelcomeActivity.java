a0980e6d212aaed866d61e17ae74936869bd95ad
liying <872904178@qq.com>
2017-05-01 20:16:01 +0800

    优化低版本适配

--- app/src/main/java/com/example/wangchang/testbottomnavigationbar/activity/WelcomeActivity.java
+++ app/src/main/java/com/example/wangchang/testbottomnavigationbar/activity/WelcomeActivity.java
@@ -5,14 +5,14 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.wangchang.testbottomnavigationbar.R;
 import com.zhy.m.permission.MPermissions;
@@ -108,20 +108,24 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 设置无标题
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 设置全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
         /**
          * 完全沉浸式
          */
-        if (Build.VERSION.SDK_INT >= 21) {
+        /*if (Build.VERSION.SDK_INT >= 21) {
             View decorView = getWindow().getDecorView();
-            /*int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            *//*int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;*/
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;*//*
             int option = View.SYSTEM_UI_FLAG_FULLSCREEN;
             decorView.setSystemUiVisibility(option);
             getWindow().setNavigationBarColor(Color.TRANSPARENT);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
-        }
+        }*/
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null)
            actionBar.hide();
