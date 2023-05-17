e9b5095e7c375cb5aeaf462cba2ea6440b37254a
liying <tjuciliying@163.com>
2023-05-17 13:55:44 +0800

    .gitignore

--- app/src/main/java/com/example/wangchang/testbottomnavigationbar/enity/Item.java
+++
@@ -1,8 +0,0 @@
-// (c)2016 Flipboard Inc, All Rights Reserved.
-
-package com.example.wangchang.testbottomnavigationbar.enity;
-
-public class Item {
-    public String description;
-    public String imageUrl;
-}
--- app/src/main/java/com/example/wangchang/testbottomnavigationbar/http/ProgressCancelListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.wangchang.testbottomnavigationbar.http;
-
-/**
- * Created by helin on 2016/10/10 15:50.
- */
-
-public interface ProgressCancelListener {
-    void onCancelProgress();
-}
